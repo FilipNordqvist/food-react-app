5ae646253f5fadbecc1975064f881978ac0cc65b
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-16 13:47:29 +0200

    Ändra namn

--- food-app/src/components/Recipe/index.js
+++ food-app/src/components/Recipe/index.js
@@ -3,12 +3,15 @@ import styles from './recipe.module.css';
 
 
 const Recipe = ({recipe, searchRecipe, addFavorite}) => {
+   
+
     return (
         <div className = "card col-md-6 col-lg-3 shadow p-3 mb-5 bg-light rounded" data-grade = {recipe.grade} data-title= {recipe.title}>
             <img src={recipe.image} className="card-img-top" alt={recipe.title}></img>
             <div className="card-body">
             <h5 className="card-title">{recipe.title}</h5>
                 <div className="d-flex justify-content-center"> 
+            
                     <button className="btn btn-primary " onClick={() => searchRecipe(recipe.id)}>
                     View recipe
                     </button>  
