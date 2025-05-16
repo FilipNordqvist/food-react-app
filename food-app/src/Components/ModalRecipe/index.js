abf856a2ad073b789212da5e4c73caa5ec33c0b5
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-16 16:02:04 +0200

    Styling modul

--- food-app/src/Components/ModalRecipe/index.js
+++ food-app/src/Components/ModalRecipe/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import './modal.module.css';
+import styles from './modal.module.css';
 
 const Modal = ({ingredients, steps, closeModal, title}) => {
     return (
         <div className="modal show d-block">
             <div className="modal-dialog modal-dialog-scrollable">
-                <div className="modal-content">
+                <div className="modal-content" style={{ maxWidth: '700px', maxHeight: '700px' }}>
                     <div className="modal-header">
-                        <h3 className="modal-title">{title}</h3>
+                        <h3 className="modal-title text-center w-100">{title}</h3>
                         <button
                             type="button"
                             className="btn-close"
@@ -18,9 +18,9 @@ const Modal = ({ingredients, steps, closeModal, title}) => {
                     <div className="modal-body">
                         
                         <div className="row">
-                            <div className="col-8 col-sm-6">
+                            <div className="col-8 col-sm-6 text-center w-50">
                                 <h5><strong>Ingredients</strong></h5>{ingredients}</div>
-                             <div className="col-8 col-sm-6"><h5><strong>Instructions</strong></h5>{ steps }</div>
+                             <div className="col-8 col-sm-6 text-center w-50"><h5><strong>Instructions</strong></h5>{ steps }</div>
                             
                             
                             </div>
