fd060d23838856bfa4d94a77a0e173533974c7f2
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2025-05-16 17:11:40 +0200

    Fixat id för Checkbox-grejer

--- food-app/src/components/Checkbox/index.js
+++ food-app/src/components/Checkbox/index.js
@@ -2,8 +2,8 @@
 const Checkbox = ({value, id}) => {
     return(
         <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value={value}></input>
-            <label className="form-check-label" htmlFor="inlineCheckbox1">{value}</label>
+            <input className="form-check-input" type="checkbox" id={id} value={value}></input>
+            <label className="form-check-label" htmlFor={id}>{value}</label>
         </div>
         
     );
