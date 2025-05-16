278c55a086d379063eac55519f4ef12ca0cc68fb
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2025-05-16 14:15:14 +0200

    Fixat storleken på alla Cards

--- food-app/src/components/Recipe/index.js
+++ food-app/src/components/Recipe/index.js
@@ -4,12 +4,12 @@ import styles from './recipe.module.css';
 
 const Recipe = ({recipe, searchRecipe, addFavorite}) => {
     return (
-        <div className = "card shadow p-3 mb-5 bg-light rounded h-100" data-grade = {recipe.grade} data-title= {recipe.title}>
+        <div className = "card shadow p-3 mb-5 bg-light rounded" data-grade = {recipe.grade} data-title= {recipe.title}>
             <img src={recipe.image} className="card-img-top" alt={recipe.title}></img>
-            <div className="card-body">
-            <h5 className="card-title">{recipe.title}</h5>
+            <div className="card-body w-100 h-100">
+                <h5 className="card-title">{recipe.title}</h5>
                 <div className="d-flex justify-content-center"> 
-                    <button className="btn btn-primary " onClick={() => searchRecipe(recipe.id)}>
+                    <button className="btn btn-primary" onClick={() => searchRecipe(recipe.id)}>
                     View recipe
                     </button>  
                     <img src="/images/star.png" alt="Favorite recipe" className={styles.favorite} onClick={ () => addFavorite(recipe.title)}></img>
--- food-app/src/components/RecipeList/index.js
+++ food-app/src/components/RecipeList/index.js
@@ -5,12 +5,10 @@ import styles from './recipelist.module.css';
 const RecipeList = ({recipes, searchRecipe}) => {
     return(
         <div className="container mt-3">
-            <div className="row">
+            <div className="row g-4 align-items-stretch">
             {recipes.map((recipe) => (
-                <div key={recipe.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card w-100 h-100">
-                <Recipe key={recipe.id} recipe={recipe} searchRecipe={searchRecipe} />
-                    </div>
+                <div key={recipe.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex">                    
+                <Recipe key={recipe.id} recipe={recipe} searchRecipe={searchRecipe} />                    
                 </div>
             ))}
             </div>
