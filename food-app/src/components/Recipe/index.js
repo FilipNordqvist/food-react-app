590c2f66546111e551e97803bd8272ddb4c7a860
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2025-05-16 11:16:14 +0200

    Tog bort stjärnorna i Recipe

--- food-app/src/components/Recipe/index.js
+++ food-app/src/components/Recipe/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
 import styles from './recipe.module.css';
 
 const Recipe = ({recipe, onDelete}) => {
-    const stars = [];
-    for(let i = 0; i < recipe.grade; i++){
-        stars.push(<img src='/images/star.png' alt='Star' className={styles.img} key={i}></img>);
-    }
+
     return (
         <div className = "card col-md-6 col-lg-3" data-grade = {recipe.grade} data-title= {recipe.title}>
             <img src={recipe.image} className="card-img-top" alt={recipe.title}></img>
@@ -13,7 +9,6 @@ const Recipe = ({recipe, onDelete}) => {
             <h5 className="card-title">{recipe.title}</h5>
             <a href="#" class="btn btn-primary">Go somewhere</a>       
         <img src="/images/delete.png" alt="Delete movie" className={styles.delete} onClick={ () => onDelete(recipe.title)}></img>
-        {stars}
             </div>
         </div>
 
