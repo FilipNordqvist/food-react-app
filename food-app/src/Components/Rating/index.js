ffcd145d37819bd87f0df6d66a2f84e61a52f0cd
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-19 13:33:23 +0200

    La till css fil med

--- food-app/src/Components/Rating/index.js
+++ food-app/src/Components/Rating/index.js
@@ -1,26 +1,24 @@
 import React from "react";
 
-const Rating = ({id, value, onChange}) => {
-    return(
-         
-         <>
-         <label htmlFor={id}>Betyg:</label>
-            <select 
-            id={id} 
-            className="form-control" 
-            value={value}
-            onChange={(e) => onChange (Number(e.target.value))}
-            >
-                
-            <option value="0">Välj betyg här...</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            </select>
-            </>
-    );
+const Rating = ({ id, value, onChange }) => {
+  return (
+    <>
+      <label htmlFor={id}>Betyg:</label>
+      <select
+        id={id}
+        className="form-control"
+        value={value}
+        onChange={(e) => onChange(Number(e.target.value))}
+      >
+        <option value="0">Välj betyg här...</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </>
+  );
 };
 
 export default Rating;
