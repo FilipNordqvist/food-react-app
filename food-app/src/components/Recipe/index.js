e30b0e9ce0566036e7aad4b6f7acfcee632ab512
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-19 12:44:11 +0200

    Revert "Merge branch 'Sort-instructions-recipe'"
    This reverts commit 94e992f4ce0f9c89d94154e06541a9fca3107150, reversing
    changes made to c179076519268402d74d6f94914f5542662c066c.

--- food-app/src/components/Recipe/index.js
+++ food-app/src/components/Recipe/index.js
@@ -1,65 +1,61 @@
-import React, { useState } from "react";
-import styles from "./recipe.module.css";
-import ModalRecipe from "../ModalRecipe";
+import React, {useState} from 'react';
+import styles from './recipe.module.css';
+import ModalRecipe from '../ModalRecipe';
 
-const Recipe = ({ recipe, addFavorite }) => {
-  const [modal, setModal] = useState(false);
 
-  const openModal = () => setModal(true);
-  const closeModal = () => setModal(false);
 
-  const ingredients = [];
-  const instructions = [];
+const Recipe = ({recipe, handleItem, image}) => {
+    const [modal, setModal] = useState(false);
+    
+    const openModal = () => setModal(true);
+    const closeModal = () => setModal(false);
+    
+    const ingredients = [];
+    const steps = recipe.analyzedInstructions[0].steps[0].step;
+ 
 
-  try {
-    let steps = recipe.analyzedInstructions[0].steps;
-    for (let i = 0; i < steps.length; i++) {
-      instructions.push(<p key={i}>{steps[i].step}</p>);
+
+    for (let i = 0; i < recipe.extendedIngredients.length; i++) {
+        ingredients.push(<p key={i}>{recipe.extendedIngredients[i].original}</p>);
     }
-  } catch {
-    instructions[0] = "No instructions";
-  }
 
-  for (let i = 0; i < recipe.extendedIngredients.length; i++) {
-    ingredients.push(<p key={i}>{recipe.extendedIngredients[i].original}</p>);
-  }
+    return (
+        
+        <>
+            <div 
+                className="card shadow p-3 mb-5 bg-light rounded" 
+                onClick={openModal} 
+                
+                data-title={recipe.title}
+                style={{ cursor: 'pointer' }}
+            >
+                <img src={recipe.image} className="card-img-top" alt={recipe.title} />
+                <div className="card-body d-flex flex-column flex-grow-1 w-100 h-100">
+                    <h4 className="card-title">{recipe.title}</h4>
+                    <div className="mt-auto d-flex justify-content-center">
+                        <img 
+                            src={image} 
+                            alt="Favorite recipe" 
+                            className={styles.favorite} 
+                            onClick={(e) => {
+                                e.stopPropagation(); // Hindra modalen från att öppnas vid klick
+                                handleItem(recipe);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
 
-  return (
-    <>
-      <div
-        className="card shadow p-3 mb-5 bg-light rounded"
-        onClick={openModal}
-        data-grade={recipe.grade}
-        data-title={recipe.title}
-        style={{ cursor: "pointer" }}
-      >
-        <img src={recipe.image} className="card-img-top" alt={recipe.title} />
-        <div className="card-body w-100 h-100">
-          <h4 className="card-title">{recipe.title}</h4>
-          <div className="d-flex justify-content-center">
-            <img
-              src="/images/star.png"
-              alt="Favorite recipe"
-              className={styles.favorite}
-              onClick={(e) => {
-                e.stopPropagation(); // Hindra modalen från att öppnas vid klick
-                addFavorite(recipe.title);
-              }}
-            />
-          </div>
-        </div>
-      </div>
-
-      {modal && (
-        <ModalRecipe
-          steps={instructions}
-          ingredients={ingredients}
-          closeModal={closeModal}
-          title={recipe.title}
-        />
-      )}
-    </>
-  );
+            {modal && (
+                <ModalRecipe
+                    steps={steps}
+                    ingredients={ingredients}
+                    closeModal={closeModal}
+                    title={recipe.title}
+                />
+            )}
+        </>
+    );
 };
 
 export default Recipe;
