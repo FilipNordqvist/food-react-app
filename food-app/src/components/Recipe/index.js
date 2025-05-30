27d4ed825b007f6dcd010fe2fd554d56f329bc07
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-05-30 10:37:55 +0200

    Försöker fixa main

--- food-app/src/components/Recipe/index.js
+++ food-app/src/components/Recipe/index.js
@@ -17,18 +17,12 @@ const Recipe = ({
   const openModal = () => setModal(true);
   const closeModal = () => setModal(false);
 
-const Recipe = ({recipe, handleItem, image, showRating= false}) => {
-    const [modal, setModal] = useState(false);
-    const [rating, setRating] = useState(0);
-    
-    const openModal = () => setModal(true);
-    const closeModal = () => setModal(false);
-    
-    const ingredients = [];
+  const ingredients = [];
   const instructions = [];
+  let steps = [];
 
   try {
-    let steps = recipe.analyzedInstructions[0].steps;
+    steps = recipe.analyzedInstructions[0].steps;
     for (let i = 0; i < steps.length; i++) {
       instructions.push(<p key={i}>{steps[i].step}</p>);
     }
@@ -40,67 +34,33 @@ const Recipe = ({recipe, handleItem, image, showRating= false}) => {
     ingredients.push(<p key={i}>{recipe.extendedIngredients[i].original}</p>);
   }
 
+  const handleRating = (value) => {
+    setRating(value);
+    recipe.rating = value;
+    const storedRating = localStorage.getItem("recipeRatings");
+    let ratings = {};
+    if (storedRating) {
+      ratings = JSON.parse(storedRating);
+    }
     ratings[recipe.id] = value;
     localStorage.setItem("recipeRatings", JSON.stringify(ratings));
   };
 
-    const handleRating = (value) => {
-        setRating(value);
-        recipe.rating = value;
-        const storedRating = localStorage.getItem("recipeRatings");
-        let ratings = {};
-        if (storedRating) {
-            ratings = JSON.parse(storedRating);
-        }
-
-        ratings[recipe.id] = value;
-        localStorage.setItem("recipeRatings", JSON.stringify(ratings));
-    };
-
-    return (
-        
-        <>
-            <div 
-                className="card shadow bg-light rounded d-flex flex-column h-100 w-100"
-                onClick={openModal} 
-                
-                data-title={recipe.title}
-                style={{ cursor: 'pointer' }}
-            >
-                <img src={recipe.image} className="card-img-top" alt={recipe.title} />
-                <div className="card-body d-flex flex-column flex-grow-1">
-                    <h4 className="card-title">{recipe.title}</h4>
-                    <div className="flex-grow-1"></div>
-                    <div>{recipe.readyInMinutes} min</div>
-                    {showRating && (
-                    <div onClick={(e) => e.stopPropagation()} className='mt-3'>
-                        <Rating
-                        id={`rating-${recipe.id}`}
-                        value={rating}
-                        onChange={handleRating}
-                        />
-
-                    </div>
-                    )}
-
-                    <div className="mt-auto d-flex justify-content-center">
-                        <img 
-                            src={image} 
-                            alt="Favorite recipe" 
-                            className={styles.favorite} 
-                            onClick={(e) => {
-                                e.stopPropagation(); // Hindra modalen från att öppnas vid klick
-                                handleItem(recipe, rating);
-                            }}
-                        />
-                    </div>
-                    
-                </div>
-            </div>
-          )}
-
+  return (
+    <>
+      <div
+        className="card shadow bg-light rounded d-flex flex-column h-100 w-100"
+        onClick={openModal}
+        data-title={recipe.title}
+        style={{ cursor: 'pointer' }}
+      >
+        <img src={recipe.image} className="card-img-top" alt={recipe.title} />
+        <div className="card-body d-flex flex-column flex-grow-1">
+          <h4 className="card-title">{recipe.title}</h4>
+          <div className="flex-grow-1"></div>
+          <div>{recipe.readyInMinutes} min</div>
           {showRating && (
-            <div onClick={(e) => e.stopPropagation()} className="mt-3">
+            <div onClick={(e) => e.stopPropagation()} className='mt-3'>
               <Rating
                 id={`rating-${recipe.id}`}
                 value={rating}
